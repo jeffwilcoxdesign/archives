3e2089c67886e04847e593f7b6ef127e41a6bad6
Jeffrey Wilcox <jeff@Jeffreys-Mac-mini.local>
2019-09-16 19:00:13 -0700

    default create-react-app project setup and build

---
+++ build/precache-manifest.875a728f92d7692b27094720069bb156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ce8599edd806ff6b6a9f006a113da0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed6a6a3cfe36395b99fe",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "68da079bb8f95ab621b5",
+    "url": "/static/js/2.8670806f.chunk.js"
+  },
+  {
+    "revision": "ed6a6a3cfe36395b99fe",
+    "url": "/static/js/main.80585b33.chunk.js"
+  },
+  {
+    "revision": "5e2010afda9cf521fde6",
+    "url": "/static/js/runtime~main.fc86d6c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
